e689f17c3505efd61751f0ee89b07b43a31f8075
migs39 <monkey.d.migs@gmail.com>
2025-08-21 13:11:04 -0300

    fixing variable name not in camel case

--- restservice/src/main/java/com/guides/restservice/NumberData.java
+++ restservice/src/main/java/com/guides/restservice/NumberData.java
@@ -1,4 +1,4 @@
 package com.guides.restservice;
 
-public record NumberData(long requestId, int number, Boolean isPrime, Boolean isEven, int NumberOfDivisors) {
+public record NumberData(long requestId, int number, Boolean isPrime, Boolean isEven, int numberOfDivisors) {
 }
