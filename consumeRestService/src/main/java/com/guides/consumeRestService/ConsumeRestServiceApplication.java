2eaf94a0d2f05d2e081455b16a22a227e5659bf0
migs39 <monkey.d.migs@gmail.com>
2025-08-21 13:28:01 -0300

    making the application to consume the rest api on localhost:8080 numberData

--- consumeRestService/src/main/java/com/guides/consumeRestService/ConsumeRestServiceApplication.java
+++ consumeRestService/src/main/java/com/guides/consumeRestService/ConsumeRestServiceApplication.java
@@ -1,13 +1,36 @@
 package com.guides.consumeRestService;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ConsumeRestServiceApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(ConsumeRestServiceApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConsumeRestServiceApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder){
+		return builder.build();
+	}
+
+	@Bean
+	@Profile("!test")
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception{
+		return args -> {
+			DivisorsCount divisorsCount = restTemplate.getForObject(
+				"http://localhost:8080/NumberData?number=random", DivisorsCount.class);
+			log.info(String.valueOf(divisorsCount.divisorsCount()));
+		};
+	}
 }
