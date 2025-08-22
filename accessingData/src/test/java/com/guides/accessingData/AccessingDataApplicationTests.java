5297be234dc245d82bbae86c8d6a9c8310f10a87
migs39 <monkey.d.migs@gmail.com>
2025-08-22 02:49:49 -0300

    created base repository with initializr

---
+++ accessingData/src/test/java/com/guides/accessingData/AccessingDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guides.accessingData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessingDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
